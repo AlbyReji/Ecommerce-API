a6fb14e97ec61c8f7c7a389bbec3ed68ebb87a73
AlbyReji <albymathew30@gmail.com>
2023-08-18 19:44:51 +0530

    Ecommerce

--- Ecommerce/user_api/admin.py
+++ Ecommerce/user_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from user_api.models import (User,
+                    Address,
+                    UserProfile,
+                    CartItem,
+                    Order,
+                    OrderItem)
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Address)
+admin.site.register(UserProfile)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
+
